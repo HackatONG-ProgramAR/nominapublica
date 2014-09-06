32006962a8b69faba58101931ab0e5d9e4230954
Pablo Ambrosio <pandres@gmail.com>
2014-09-06 15:11:55 -0300

    app.py formateo.

--- app.py
+++ app.py
@@ -12,19 +12,19 @@ from sqlalchemy import Float
 import json
 
 class Entity(object):
-  @property
-  def as_dict(self):
-    d = {}
-    for column in self.__table__.columns:
-      attr = getattr(self, column.name)
-      if attr is not None:
-        if isinstance(column.type, Float):
-          d[column.name] = float(attr)
-        else:
-          d[column.name] = unicode(attr)
-      else:
-        d[column.name] = None
-    return d
+    @property
+    def as_dict(self):
+        d = {}
+        for column in self.__table__.columns:
+          attr = getattr(self, column.name)
+          if attr is not None:
+            if isinstance(column.type, Float):
+              d[column.name] = float(attr)
+            else:
+              d[column.name] = unicode(attr)
+          else:
+            d[column.name] = None
+        return d
 
 class Persona(db.Model, Entity):
     __tablename__ = 'personas'
